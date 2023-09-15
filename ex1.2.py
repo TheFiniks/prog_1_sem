442cc339b3bae48d74495f34ca8ae5db3d3ed3a3
TheFiniks <iskhakov.tr@phystech.edu>
2023-09-15 18:11:55 +0300

    first commit

--- ex1.2.py
+++ ex1.2.py
@@ -1,3 +1,6 @@
-a, b = input().split()
-c = float(a) ** int(b)
-print(round(c, 6))
+numbers = list(map(int, input().split()))
+last_number = numbers[0]
+for i in range(1, last_number + 1):
+    count_ = numbers[1:].count(i)
+    if count_ == 0:
+        print(i)
